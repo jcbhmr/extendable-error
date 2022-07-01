8fb9e3123119d26712afaca19c1970f1285a994b
Jacob Hummer <jcbhmr@outlook.com>
2022-06-30 22:51:47 -0500

    Change up build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-#! /usr/bin/env bash
-set -eu
+#! /usr/bin/env -S bash -eu
+# -S makes it so the rest of the command is treated as a single arg, not more 'env -v' args
+# -eu makes bash error on undefined $VARIABLEs and exit on any non-zero exit code
 
+# Use tsconfig.json for config
 npx tsc
