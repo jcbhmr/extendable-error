7d13c432de9d4c275c03e7ae9108068ebc41b96a
Jacob Hummer <61068799+jcbhmr@users.noreply.github.com>
2022-07-01 03:38:19 +0000

    Add a bunch of stuff

--- -.ts
+++ -.ts
@@ -1,4 +1,9 @@
 class ExtendableError extends Error {
+	// Preset the name to be the @@toStringTag but fallback to the
+	// classes .name property
+	// TODO: Should we use new.target or this.constructor?
+	name = this[Symbol.toStringTag] ?? this.constructor.name
+	
     // Preserve that Error.length is 1
 	constructor(a, ...rest) {
         // Error if not extended! This is an abstract-like class that should
@@ -10,13 +15,10 @@ class ExtendableError extends Error {
 
 		super(a, ...rest)
 
-		// Preset the name to be the @@toStringTag but fallback to the
-        // classes .name property
-        // TODO: Should we use new.target or this.constructor?
-        this.name = this[Symbol.toStringTag] ?? this.constructor.name
-
         // Hide internal stack trace if possible
+		// @ts-ignore TS doesn't know about V8 captureStackTrace()
 		if (typeof Error.captureStackTrace === "function") {
+			// @ts-ignore
 			Error.captureStackTrace(this, this.constructor)
 		}
 	}
