62ce822ca6aeb3adee2ab2d6b5248c59ae948509
jcbhmr <>
2022-07-01 03:58:11 +0000

    Remove .sh files to re-add with +x permission

--- build.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env -S bash -eu
-# -S makes it so the rest of the command is treated as a single arg, not more 'env -v' args
-# -eu makes bash error on undefined $VARIABLEs and exit on any non-zero exit code
-
-# Use tsconfig.json for config
-npx tsc
